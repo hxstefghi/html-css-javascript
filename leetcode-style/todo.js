93d462fff10793de03268489d973d793799d736c
hxstefghi <cccatuday1401qc@student.fatima.edu.ph>
2026-05-15 22:54:59 +0800

    refactor task rendering and event attachment in to-do list functionality

--- leetcode-style/todo.js
+++ leetcode-style/todo.js
@@ -14,19 +14,7 @@ taskBtn.addEventListener("click", function () {
   let taskItem = taskInput.value;
   addTaskToArray(taskItem);
   renderTask(tasks);
-
-  let outputs = document.getElementsByClassName("tasksOutput");
-
-  for (let i = 0; i < outputs.length; i++) {
-    outputs[i].addEventListener("click", function () {
-      tasks[i].completed = !tasks[i].completed;
-    });
-
-    if (tasks[i].completed) {
-      outputs[i].classList.add("line-through");
-    }
-    renderTask(tasks);
-  }
+  attachTaskEvents();
 });
 
 function addTaskToArray(taskItem) {
@@ -43,8 +31,23 @@ function renderTask(tasks) {
   let output = "";
 
   for (let i = 0; i < tasks.length; i++) {
-    output += "<p class='tasksOutput'>" + tasks[i].text + "</p>";
+    if (tasks[i].completed) {
+      output += "<p class='tasksOutput line-through'>" + tasks[i].text + "</p>";
+    } else {
+      output += "<p class='tasksOutput'>" + tasks[i].text + "</p>";
+    }
   }
 
   display.innerHTML = output;
+}
+
+function attachTaskEvents() {
+  let outputs = document.getElementsByClassName("tasksOutput");
+
+  for (let i = 0; i < outputs.length; i++) {
+    outputs[i].addEventListener("click", function () {
+      tasks[i].completed = !tasks[i].completed;
+      renderTask(tasks);
+    });
+  }
 }
