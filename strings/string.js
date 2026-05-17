40f72ed4a36a979541434dd2e5692164d85f1bcc
hxstefghi <cccatuday1401qc@student.fatima.edu.ph>
2026-05-17 23:01:04 +0800

    fix: update greeting string and enhance string search methods

--- strings/string.js
+++ strings/string.js
@@ -27,7 +27,7 @@ let num = "9";
 console.log(num.padEnd(4, "0"));
 console.log(num.repeat(5));
 
-let greet = "Good morning, Christian!";
+let greet = "Good morning, Christian";
 console.log(greet);
 console.log(greet.replace("morning", "afternoon"));
 
@@ -36,5 +36,14 @@ console.log(greet.replace("morning", "afternoon"));
 // greet.indexOf("morning");
 console.log(greet.indexOf("morning", 2));
 console.log(greet.search("morning"));
+// console.log(Array.isArray(greet));
+console.log(greet.includes("Good"));
+console.log(greet.endsWith(",", 13));
+console.log(greet.startsWith("good"));
+console.log(greet.repeat(5));
+let num2 = new Uint32Array(10);
+console.log(num2);
+console.log(navigator.geolocation);
 console.log(Array.isArray(greet));
-Array.from()
+
+
